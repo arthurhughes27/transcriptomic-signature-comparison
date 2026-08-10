debef180c81b13c874948e965bac2c976bf4e2bc
Arthur Hughes <arthur.hughes27@outlook.com>
2026-08-10 12:03:57 +0200

    aesthetics

--- R/baseline_comparison_plots.R
+++ R/baseline_comparison_plots.R
@@ -79,13 +79,9 @@ plot_baseline_significance_comparison <- function(pct_df,
     ggplot2::theme_minimal() +
     ggplot2::theme(
       panel.grid.minor  = ggplot2::element_blank(),
-      strip.text         = ggplot2::element_text(face = "bold", size = 14),
+      strip.text         = ggplot2::element_text(face = "bold"),
       panel.spacing.x     = grid::unit(14, "pt"),
-      axis.text.x         = ggplot2::element_text(angle = 45, hjust = 1),
-      axis.title.x = element_text(size = 17),
-      axis.title.y = element_text(size = 17),
-      axis.text.y         = ggplot2::element_text(size = 12),
-      plot.title = element_text(size = 17, face = "bold"),
+      axis.text.x         = ggplot2::element_text(angle = 45, hjust = 1)
     ) +
     ggplot2::labs(
       x     = "Vaccine",
